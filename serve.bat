dd12a2501624588f11c600082d0f18e73918911b
lc0-chess <77932716+lc0-chess@users.noreply.github.com>
2021-01-25 09:03:34 +0000

    Update serve.bat

--- serve.bat
+++ serve.bat
@@ -7,5 +7,5 @@ set ENGINE_MOVE_OVERHEAD=5000
 set WELCOME_MESSAGE=Lc0 v0.26.3
 set ALLOW_PONDER=true
 set PGN_URL=https://raw.githubusercontent.com/roaringroars/botpgnrepo/main/Carlsen.pgn
-
+set DISABLE_BOT=true
 node server.js
